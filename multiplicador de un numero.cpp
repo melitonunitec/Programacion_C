fd1631886d852dcf0503ce94d01df44205d3a5a3
melitonunitec <m_eli03@hotmail.com>
2021-07-30 11:57:06 -0500

    Entorno de lenguaje en C (#1)
    Control de flujo  y estructura sencillas de operadores ,facil de entender

---
+++ multiplicador de un numero.cpp
@@ -0,0 +1,17 @@
+//condicion while 
+//tabla de multiplicar de un numero
+#include <stdio.h>
+ main(){
+int n,i;
+printf("introduzca un numero:");
+scanf("%d", &n);
+i=0;//contador
+while(i<10){
+	printf("%d x %d=%d\n",n,i,n*i);
+	i++;//variable de control
+	
+}	
+	
+	
+	
+}
---
+++ numeros positivos.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+main(){
+ int x;
+ printf("teclea u numero");
+ scanf("%d" ,&x);
+ if(x>0){
+ 	printf("El numero %d es positivo",x);
+}
+ return 0;
+}
